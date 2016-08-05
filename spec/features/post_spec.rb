c5c64a0dedbb6ef1f6ab0a74eef6e075c789ab7d
LiuJing <graceliu@thoughtworks.com>
2016-08-05 12:55:16 +0800

    add create edit test

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,21 +1,37 @@
 require 'rails_helper'
 
 RSpec.feature 'post feature', type: :feature do
-  scenario 'create new post' do
+  scenario 'not create post if cancel' do
     visit '/posts'
-    sleep(1)
-    # expect(page).not_to have_content('initialize')
-    # expect(page).to have_content('start loading...')
-    # expect(page).to have_content('finish loading')
     click_link('New Post')
-    fill_in('post_title', with: 'my first post')
-    sleep(1)
-    fill_in('post_content', with: 'I wanna be the full-stack dev')
-    sleep(1)
-    click_button('Create Post')
-    sleep(1)
+    fill_in('post_title', with: 'Grace first post')
+    fill_in('post_content', with: 'I wanna be shaonainai')
+    click_link('Back')
+    expect(page).not_to have_content('Grace first post')
+  end
+
+  scenario 'create new post successfully' do
     visit '/posts'
-    sleep(1)
-    expect(page).to have_content('my first post')
+    click_link('New Post')
+    fill_in('post_title', with: 'Grace first post')
+    fill_in('post_content', with: 'I wanna be shaonainai')
+    click_button('Create Post')
+    expect(page).to have_content('Post was successfully created.')
+    visit '/posts'
+    expect(page).to have_content('Grace first post')
+    expect(page.find('.top-10-container')).to have_content('Grace first post')
+  end
+
+  scenario 'edit post successfully' do
+    visit '/posts'
+    click_link('New Post')
+    fill_in('post_title', with: 'Grace first post')
+    fill_in('post_content', with: 'I wanna be shaonainai')
+    click_button('Create Post')
+    click_link('Edit')
+    fill_in('post_title', with: 'Grace second post')
+    fill_in('post_content', with: 'I wanna be dev')
+    click_button('Update Post')
+    expect(page).to have_content('Post was successfully updated.')
   end
 end
